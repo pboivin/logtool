f4ff35cbf5d6af19cdfcb1ec36f0fb6b33f5e92e
Patrick Boivin <pboivin@gmail.com>
2023-04-07 16:04:53 -0400

    Setup testing

---
+++ tests/EntryTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pboivin\Logtool\Tests;
+
+use Pboivin\Logtool\Entry;
+use PHPUnit\Framework\TestCase;
+
+class EntryTest extends TestCase
+{
+    public function test_can_initialize_entry()
+    {
+        $entry = new Entry("[2001-01-01] Test\n\n");
+        $entry->addLine("One\n\n");
+        $entry->addLine("Two\n\n");
+        $entry->addLine("Three\n\n");
+
+        $this->assertEquals('[2001-01-01] Test', $entry->header());
+        $this->assertEquals('2001-01-01', $entry->date());
+        $this->assertEquals(3, count($entry->body()));
+        $this->assertEquals("[2001-01-01] Test\nOne\nTwo\nThree\n\n", (string) $entry);
+    }
+}
---
+++ tests/LogtoolTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pboivin\Logtool\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class LogtoolTest extends TestCase
+{
+    public function test_it_works()
+    {
+        $this->assertTrue(!!1);
+    }
+}
